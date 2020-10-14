56722bb72fecbd33cde9662813a37eaced518d4f
jordyngoma <ngoma.jordy@yahoo.com>
2020-10-14 15:54:10 +0200

    completer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,18 @@
 import React from 'react';
 const name = 'Gérard';
 const avatar = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg';
-const online = true ;
-function Contact (){
+const online = false ;
+
+function Contact ({name,avatar,online}){
     return(
         <div className="Contact">
     <img class='avatar' src = {avatar} />
-    <div class='ajust'>
+    <div class='status'>
     <div class='name'>{name}</div>
-    
-    <div class='status'>{online ? "Online":"Offline"}</div>
+    <span class= {online ? "status-online":"status-offline"}></span>
+    <div class='status-text'>{online ? "Online":"Offline"}</div>
       </div>
-  </div>
+    </div>
     )
 }
 
