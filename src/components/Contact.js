12757c482acc67d4f5fe4754607384b28922e8cd
jordyngoma <68231131+jordyngoma@users.noreply.github.com>
2020-10-21 16:52:41 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,17 +3,29 @@ const name = 'Gérard';
 const avatar = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/chat_avatar_01.jpg';
 const online = false ;
 
-function Contact ({name,avatar,online}){
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      online:false
+    };
+  }
+  render(){
     return(
         <div className="Contact">
-    <img class='avatar' src = {avatar} />
+    <img class='avatar' src = {this.props.avatar} />
     <div class='status'>
-    <div class='name'>{name}</div>
-    <span class= {online ? "status-online":"status-offline"}></span>
-    <div class='status-text'>{online ? "Online":"Offline"}</div>
+    <div class='name'>{this.props.name}</div>
+    <span class= {this.state.online ? "status-online":"status-offline"}></span>
+    <div class='status-text' onClick = { event => {
+      const newOnline = !this.state.online;
+      this.setState({ online : newOnline });
+    }}
+    >{this.state.online ? "Online":"Offline"}</div>
       </div>
     </div>
     )
+    }
 }
 
 export default Contact;
